992b138e753fa2f5b8cf77f8609ddaa5c096bc97
quang vuong <quangvuong12c7@gmail.com>
2018-06-03 22:55:17 +0700

    HienKetQua

--- LoiKhuyenAnUong/FormBMI.cs
+++ LoiKhuyenAnUong/FormBMI.cs
@@ -64,7 +64,34 @@ namespace LoiKhuyenAnUong
         // hàm hiện kết quả
         private void HienKetQua(double ChiSoBMI)
         {
-            
+            String TheTrang = "";
+
+            if (ChiSoBMI < 18.5)
+            {
+                TheTrang = "Gầy";
+            }
+            else if (ChiSoBMI >= 18.5 && ChiSoBMI <= 24.9)
+            {
+                TheTrang = "Bình Thường";
+            }
+            else if (ChiSoBMI >= 25.0 && ChiSoBMI <= 29.9)
+            {
+                TheTrang = "Hơi Béo";
+            }
+            else if (ChiSoBMI >= 30.0 && ChiSoBMI <= 34.9)
+            {
+                TheTrang = "Béo Phì Cấp Độ 1";
+            }
+            else if (ChiSoBMI >= 35.0 && ChiSoBMI <= 39.9)
+            {
+                TheTrang = "Béo Phì Cấp Độ 2";
+            }
+            else
+            {
+                TheTrang = "Béo Phì Cấp Độ 3";
+            }
+
+            lbChiSo.Text = "Chỉ số BMI: " + Convert.ToString(ChiSoBMI) + "\nTình trạng cơ thể: " + TheTrang;
         }
 
         private void btnLoiKhuyenToolStripMenuItem_Click(object sender, EventArgs e)
